6586d6648d911139d0322cfcc612b55c8c56a1bf
me <me@me.com>
2022-06-26 03:30:40 +1200

    making progress

--- forbidden.py
+++ forbidden.py
@@ -13,12 +13,6 @@ except AttributeError:
 
 RegexType = type(re.compile('string'))
 
-def main():
-    with open("lex2.py", "r") as source:
-        nodes = ast.parse(source.read())
-        transformer = CustomNodeTransformer()
-        transformer.visit(nodes)
-
 class CustomNodeTransformer(ast.NodeTransformer):
     variable_id = ""
     variable_value = ""
@@ -32,18 +26,16 @@ class CustomNodeTransformer(ast.NodeTransformer):
         #print(values)
 
         try:
-            variable_id = targets['id']
+            variable_id = targets.get('id')
             #print(variable_id)
-            variable_value = values['value']
+            variable_value = values.get('value')
             #print(variable_value)
-            variable_location = values['lineno']
+            variable_location = values.get('lineno')
             #print(variable_location)
 
             if isinstance(variable_value, (str, RegexType)):
                 variable = (variable_id, variable_value, variable_location)
                 self.all_variables[variable_id] = (variable_value, variable_location)
-                print("\n")
-                print(variable) 
             variable = ()
         except:
             traceback.print_exc()
@@ -53,30 +45,43 @@ class CustomNodeTransformer(ast.NodeTransformer):
 
     def visit_Constant(self, node):
         if(isinstance(node.value, str)):
-            print(f"\nplain string: {node.value}, lineno: {node.lineno}" )
+            # print(f"\nplain string: {node.value}, lineno: {node.lineno}" )
+            self.all_variables["barestring"] = (node.value)
+            print("\n")
 
-
-    def visit_args(self, node):
-        tmp = node.args
-        print("from visit args")
-        print(tmp)
 
     def visit_FunctionDef(self, node):
-        try:
-            function_args = node.args.__dict__
-            function_arguments = function_args['args'][0].arg
-            function_string_values = ""
+
+        num_of_arguments = len(node.args.__dict__.get('args'))
+
+        for i in range(0, num_of_arguments):
             try:
-                function_string_values =  function_args['defaults'][0].value
-            except IndexError:
+                function_args = node.args.__dict__.get('args')
+                function_variable_id = function_args[i].arg
+
+                strings_assigned_in_function = node.args.__dict__.get('defaults')
+                if strings_assigned_in_function[i] is not None:
+                    function_string_value = strings_assigned_in_function[i].value
+
+
+                if function_variable_id and function_string_value:
+                    self.all_variables[function_variable_id] = (function_variable_id, function_string_value)
+
+            except:
+                traceback.print_exc()
                 pass
 
-            if function_arguments and function_string_values:
-                print(f"\nfunction def args = {arguments}, values = {function_string_values}")
 
-        except:
-            traceback.print_exc()
-            pass
+    def report(self):
+        print(json.dumps(self.all_variables))
+
+def main():
+    with open("parse.py", "r") as source:
+        nodes = ast.parse(source.read())
+        transformer = CustomNodeTransformer()
+        transformer.visit(nodes)
+        transformer.report()
+
 
 if __name__ == '__main__':
     main()
